5eef71fdcade161f4818cb9e76d727750a83704a
sangjun <sangjoon12345@gmail.com>
2024-08-05 13:08:11 +0900

    upload t

---
+++ mypetshop/src/main/java/com/mypetshop/mypetshop/MypetshopApplication.java
@@ -0,0 +1,13 @@
+package com.mypetshop.mypetshop;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MypetshopApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MypetshopApplication.class, args);
+	}
+
+}
